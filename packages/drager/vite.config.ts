420d100cffb59f23723ef403a3d3c91240c3b9ef
马军 <majun@xiwicloud.com>
2024-10-07 22:02:14 +0800

    🔧 build:1

--- packages/drager/vite.config.ts
+++ packages/drager/vite.config.ts
@@ -1,32 +1,32 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
     dts({
-      tsConfigFilePath: resolve(__dirname, '../../tsconfig.json'),
-      outputDir: './lib',
-      exclude: ['./lib/**/*', 'node_modules']
+      rollupTypes: true,
+      tsconfigPath: resolve(__dirname, 'tsconfig.json') // 指定 tsconfig 文件
     }),
-    vue()
+    react()
   ],
 
   build: {
-    outDir: resolve(__dirname, 'lib'),
+    outDir: 'lib',
     lib: {
-      entry: resolve(__dirname, './index.ts'),
+      entry: resolve(__dirname, './index.tsx'),
       name: 'ESDrager',
-      fileName: (format) => `index.${format}.js`
+      fileName: 'index'
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue'],
+      external: ['react', 'react-dom'],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
-          vue: 'Vue'
+          react: 'React',
+          'react-dom': 'react-dom'
         }
       }
     }
