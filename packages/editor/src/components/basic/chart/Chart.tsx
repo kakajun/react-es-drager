6e8ed64b746dd4e97bcf266a3dee567a31ea6b29
马军 <253495832@qq.com>
2024-10-16 15:53:56 +0800

    1

--- packages/editor/src/components/basic/chart/Chart.tsx
+++ packages/editor/src/components/basic/chart/Chart.tsx
@@ -12,39 +12,39 @@ const ChartComponent: React.FC<Props> = ({ element, option }) => {
   const chartRef = useRef<HTMLDivElement>(null)
   let chart: echarts.ECharts | null = null
   const { t } = useTranslation()
+
+  function init() {
+    chart = echarts.init(chartRef.current)
+    // 绘制图表
+    chart.setOption({
+      title: {
+        text: t('examples.chartTitle')
+      },
+      tooltip: {},
+      xAxis: {
+        data: ['item1', 'item2', 'item3', 'item4', 'item5', 'item6']
+      },
+      yAxis: {},
+      series: [
+        {
+          name: 'Sales',
+          type: 'line',
+          data: [5, 20, 36, 10, 10, 20]
+        }
+      ]
+    })
+  }
+
   useEffect(() => {
-    if (chartInstance && chartRef.current) {
-      chart = echarts.init(chartRef.current)
-      chart.setOption({
-        title: {
-          text: t('examples.chartTitle')
-        },
-        tooltip: {},
-        xAxis: {
-          data: ['item1', 'item2', 'item3', 'item4', 'item5', 'item6']
-        },
-        yAxis: {},
-        series: [
-          {
-            name: 'Sales',
-            type: 'line',
-            data: [5, 20, 36, 10, 10, 20]
-          }
-        ]
-      })
-    }
+    init()
   }, [])
 
   useLayoutEffect(() => {
-    if (chartInstance) {
-      chart?.resize()
-    }
+    chart?.resize()
   }, [element.width, element.height])
 
   useEffect(() => {
-    if (chartInstance) {
-      chart?.setOption(option)
-    }
+    chart?.setOption(option)
   }, [option])
 
   const resize = () => {
