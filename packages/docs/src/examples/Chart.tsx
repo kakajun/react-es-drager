9d686836ab28c23208575d32f04c9b2738162fbb
马军 <253495832@qq.com>
2024-11-18 16:33:30 +0800

    📃 docs:改chart

--- packages/docs/src/examples/Chart.tsx
+++ packages/docs/src/examples/Chart.tsx
@@ -2,15 +2,22 @@ import React, { useState, useRef, useEffect } from 'react'
 import Drager from 'react-es-drager'
 import Chart from '@/components/Chart'
 
+// 确保 ChartType 与 ChartMethods 一致
+type ChartType = {
+  resize: () => void
+}
+
 function DragerComponent() {
   const [width, setWidth] = useState(300)
   const [height, setHeight] = useState(200)
   const [left, setLeft] = useState(100)
   const [top, setTop] = useState(100)
-  const chartRef = useRef(null)
+  const chartRef = useRef<ChartType>(null) // 确保类型与 ChartMethods 一致
 
   const handleResize = () => {
-    chartRef.current.resize()
+    if (chartRef.current) {
+      chartRef.current.resize()
+    }
   }
 
   return (
